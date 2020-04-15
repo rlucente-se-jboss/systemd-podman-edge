a77d65dc408435fd438d936bac2ff12c682820b1
rlucente-se-jboss <rlucente@redhat.com>
2020-04-15 18:56:18 -0400

    make this work on RHEL 8

--- setup.sh
+++ setup.sh
@@ -1,5 +1,45 @@
 #!/usr/bin/env bash
 
+#
+# Add your Red Hat Customer Portal credentials
+#
+USERNAME=
+PASSWORD=
+
+TYPE="Virtual"
+POOL=
+
+if [[ "$(whoami)" != "root" ]]
+then
+    echo
+    echo "*** MUST RUN AS root ***"
+    echo
+    exit 1
+fi
+
+subscription-manager register --username $USERNAME --password $PASSWORD || exit 1
+
+if [[ "$POOL" = "" ]]
+then
+    POOL=$(subscription-manager list --all --available | \
+        grep 'Pool ID\|Entitlement Type\|Subscription Name\|Available' | \
+        grep -A3 'Employee SKU' | grep -B3 $TYPE | grep 'Pool ID' | \
+        awk '{print $NF; exit}')
+
+    if [[ "$POOL" = "" ]]
+    then
+        echo "No matching pools found"
+        exit 1
+    fi
+fi
+
+subscription-manager attach --pool="$POOL" || exit 1
+
+subscription-manager repos --disable='*'
+subscription-manager repos \
+    --enable=rhel-8-for-x86_64-baseos-rpms \
+    --enable=rhel-8-for-x86_64-appstream-rpms
+
 dnf -y update
 
 dnf -y groupinstall 'Development Tools' 'Container Management'
